7af801991231c0f797b431a822231a374903779f
Ward Wouts <ward@wouts.nl>
2022-12-01 12:44:53 +0100

    bump version & nicer run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo "http://localhost:8081"
-docker rm koreader ; docker run -p 8081:8080 -v $PWD/books:/books --name koreader wardwouts/koreader-novnc
+docker rm koreader ; docker run -p 8081:8080 -v $PWD/books:/books --env EMULATE_READER_W=1000 --env EMULATE_READER_H=600 --name koreader wardwouts/koreader-novnc
