d422d6b9fab42d1fcc168450a4a1366b8f1a866f
Ward Wouts <ward@wouts.nl>
2025-04-09 18:19:02 +0200

    keep shellcheck happier

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@
 
 VERSION=$(cat VERSION)
 
-buildah manifest push --all --format v2s2 koreader-novnc docker://docker.io/wardwouts/koreader-novnc:latest
-buildah manifest push --all --format v2s2 koreader-novnc docker://docker.io/wardwouts/koreader-novnc:v$VERSION
+buildah manifest push --all --format v2s2 koreader-novnc "docker://docker.io/wardwouts/koreader-novnc:latest"
+buildah manifest push --all --format v2s2 koreader-novnc "docker://docker.io/wardwouts/koreader-novnc:v$VERSION"
