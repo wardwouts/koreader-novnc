cd8afb864da6238abf03de8061384e82494d3b78
Ward Wouts <ward@wouts.nl>
2025-08-18 15:26:43 +0200

    a little more robust for testing

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+mkdir -p /tmp/config
+
 echo "http://localhost:8081"
 echo "=== x86_64 version ==="
 podman rm koreader
-podman run -p 8081:8080 --arch=amd64 -v "$PWD/books:/books" -v "$PWD/fonts:/usr/lib/koreader/fonts/myfonts" -v "/tmp/config:/config:rw,z" --env PASSWD=-nopw --env CURSOR="" --env EMULATE_READER_W=1000 --env EMULATE_READER_H=600 --env TZ=Europe/Amsterdam --name koreader localhost/koreader-novnc
+podman run -p 8081:8080 --arch=amd64 -v "$PWD/books:/books" -v "$PWD/fonts:/usr/lib/koreader/fonts/myfonts" -v "/tmp/config:/config:rw,Z,U" --env PASSWD=-nopw --env CURSOR="" --env EMULATE_READER_W=1000 --env EMULATE_READER_H=600 --env TZ=Europe/Amsterdam --name koreader localhost/koreader-novnc
 
 echo "=== ARM64 version ==="
 podman rm koreader
-podman run -p 8081:8080 --arch=arm64 -v "$PWD/books:/books" -v "$PWD/config:/config:w" --env PASSWD=-nopw --env EMULATE_READER_W=1000 --env EMULATE_READER_H=600 --name koreader localhost/koreader-novnc
+podman run -p 8081:8080 --arch=arm64 -v "$PWD/books:/books" -v "$PWD/fonts:/usr/lib/koreader/fonts/myfonts" -v "/tmp/config:/config:rw,Z,U" --env PASSWD=-nopw --env CURSOR="" --env EMULATE_READER_W=1000 --env EMULATE_READER_H=600 --env TZ=Europe/Amsterdam --name koreader localhost/koreader-novnc
